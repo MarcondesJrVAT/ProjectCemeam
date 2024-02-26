57fd6a07bdbd68b3b35083eb68817d150824ef04
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-02-26 13:19:55 -0400

    Criando e editando tabelas para a base de dados

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar')->default('users/default.png');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('about');
+            $table->enum('status', ['ACTIVE', 'INACTIVE'])->default('ACTIVE');
+            $table->string('website');
+            $table->string('lattes');
+            $table->string('facebook');
+            $table->string('twitter');
+            $table->string('instagram');
+            $table->string('linkedin');
+            $table->string('github');
             $table->rememberToken();
             $table->timestamps();
         });
